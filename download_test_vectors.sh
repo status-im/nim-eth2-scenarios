e7ae17876fa5d5fdba10f0750bfc653f2e445032
tersec <tersec@users.noreply.github.com>
2025-06-12 14:05:15 +0000

    download v1.6.0-alpha.2-hotfix.0 test vectors and stop downloading v1.6.0-alpha.2 test vectors

--- download_test_vectors.sh
+++ download_test_vectors.sh
@@ -13,7 +13,7 @@ set -Eeuo pipefail
 if [[ -n "${CONSENSUS_TEST_VECTOR_VERSIONS:-}" ]]; then
 	IFS=',' read -ra VERSIONS <<< "$CONSENSUS_TEST_VECTOR_VERSIONS"
 else
-	VERSIONS=("v1.6.0-alpha.2")
+	VERSIONS=("v1.6.0-alpha.2-hotfix.0")
 fi
 FLAVOURS=(
 	"general"
